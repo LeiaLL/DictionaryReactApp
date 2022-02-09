a96b8867417393b8e02e1a3af32963f1378ef780
Lea Lueders <lealuders2105@gmail.com>
2022-02-09 16:44:52 +0100

    refactored dictionary code

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <Dictionary />
+          <Dictionary defaultKeyword="sunset" />
         </header>
       </div>
     </div>
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,41 +4,75 @@ import Results from "./Results";
 import Signature from "./Signature";
 import "./Dictionary.css";
 
-export default function Dictionary() {
-  const [keyword, setKeyword] = useState(" ");
+export default function Dictionary(props) {
+  console.log(props);
+  const [keyword, setKeyword] = useState(props.defaultKeyword);
   const [results, setResults] = useState(null);
+  const [ready, setReady] = useState(false);
 
   function handelResponse(response) {
     setResults(response.data[0]);
   }
 
-  function handelSubmit(event) {
-    event.preventDefault();
-
+  function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handelResponse);
+  }
+
+  function handelSubmit(event) {
+    event.preventDefault();
+    search();
   }
 
   function handelChange(event) {
     setKeyword(event.target.value);
   }
+  function load() {
+    setReady(true);
+    search();
+  }
 
-  return (
-    <div className="Dictionary">
-      <h1> Dictionary</h1>
-      <section>
-        <div className="question"> What word do you want to look up?</div>
-        <form onSubmit={handelSubmit}>
-          <input type="search" autoFocus={true} onChange={handelChange} />
-        </form>
-        <div className="suggestions">
-          {" "}
-          Suggested keywords: sunset, yoga, wine, poppy
-        </div>
-      </section>
+  if (ready) {
+    return (
+      <div className="Dictionary">
+        <h1> Dictionary</h1>
+        <section>
+          <div className="question"> What word do you want to look up?</div>
+          <form onSubmit={handelSubmit}>
+            <input
+              type="search"
+              autoFocus={true}
+              onChange={handelChange}
+              defaultValue={props.defaultKeyword}
+            />
+          </form>
+          <div className="suggestions">
+            {" "}
+            Suggested keywords: sunset, yoga, wine, poppy
+          </div>
+        </section>
 
-      <Results results={results} />
-      <Signature />
-    </div>
-  );
+        <Results results={results} />
+        <Signature />
+      </div>
+    );
+  } else {
+    load();
+    return (
+      <div className="Dictionary">
+        <h1> Dictionary</h1>
+        <section>
+          <div className="question"> What word do you want to look up?</div>
+          <form onSubmit={handelSubmit}>
+            <input type="search" autoFocus={true} onChange={handelChange} />
+          </form>
+          <div className="suggestions">
+            {" "}
+            Suggested keywords: sunset, yoga, wine, poppy
+          </div>
+        </section>
+        ;
+      </div>
+    );
+  }
 }
