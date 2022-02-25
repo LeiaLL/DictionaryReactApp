7817927e5835290fc669b66684ee4c83edaa2c03
Lea Lueders <lealuders2105@gmail.com>
2022-02-25 13:35:25 +0000

    Code maintenance (api was updated)

--- src/Phonetics.js
+++ src/Phonetics.js
@@ -3,17 +3,21 @@ import "./Phonetics.css";
 import ReactAudioPlayer from "react-audio-player";
 
 export default function Phonetics(props) {
-  return (
-    <div className="Phonetics">
-      <span>
-        <ReactAudioPlayer
-          src={props.phonetics.audio}
-          controls
-          volume={0.5}
-          className="audio"
-        />
-      </span>
-      <span className="text"> / {props.phonetics.text} /</span>
-    </div>
-  );
+  if (props.phonetics.audio && props.phonetics.text) {
+    return (
+      <div className="Phonetics">
+        <span>
+          <ReactAudioPlayer
+            src={props.phonetics.audio}
+            controls
+            volume={0.5}
+            className="audio"
+          />
+        </span>
+        <span className="text"> / {props.phonetics.text} /</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
